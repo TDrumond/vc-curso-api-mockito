82a238e35395b3ca4996b92b4b0c181e29210b56
TDrumond <thiago_drumond@outlook.com.br>
2022-11-14 18:41:27 -0300

    Teste para Classe de User Service método findById

--- src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
@@ -1,16 +1,57 @@
 package br.com.dicasdeumdev.api.services.impl;
 
+import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
+import br.com.dicasdeumdev.api.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
 class UserServiceImplTest {
+
+    public static final Integer ID = 1;
+    public static final String NAME = "Thiago Augusto";
+    public static final String EMAIL = "thiago@email.com.br";
+    public static final String PASSWORD = "123";
+    @InjectMocks
+    private UserServiceImpl service;
+    @Mock
+    private UserRepository repository;
+    @Mock
+    private ModelMapper mapper;
+
+    private User user;
+    private UserDTO userDTO;
+    private Optional<User> optionalUser;
+
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
     }
 
     @Test
-    void findById() {
+    void whenFindByIdTheReturnAnUserInstance() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+
+        User response = service.findById(ID);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+
     }
 
     @Test
@@ -27,5 +68,11 @@ class UserServiceImplTest {
 
     @Test
     void delete() {
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+        optionalUser = Optional.of(new User(ID, NAME, EMAIL, PASSWORD));
     }
 }
