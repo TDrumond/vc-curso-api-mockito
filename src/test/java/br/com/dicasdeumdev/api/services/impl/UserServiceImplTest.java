93f63e4b57c991ca8102a38266807186d0803d28
TDrumond <thiago_drumond@outlook.com.br>
2022-11-14 17:35:07 -0300

    Exemplo de teste de cobertura

---
+++ src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
@@ -0,0 +1,31 @@
+package br.com.dicasdeumdev.api.services.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
