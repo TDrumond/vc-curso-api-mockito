83ac50397849855d9b9ed55696ef5b3df2477560
TDrumond <thiago_drumond@outlook.com.br>
2022-11-15 16:28:17 -0300

    Teste para endpoint update da classe UserResource implementado.

--- src/test/java/br/com/dicasdeumdev/api/resources/UserResourceTest.java
+++ src/test/java/br/com/dicasdeumdev/api/resources/UserResourceTest.java
@@ -100,7 +100,23 @@ class UserResourceTest {
     }
 
     @Test
-    void update() {
+    void whenUpdateThenReturnSuccess() {
+        when(service.update(userDTO)).thenReturn(user);
+        when(mapper.map(any(),any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.update(ID,userDTO);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+
+
+        assertEquals(ID,response.getBody().getId());
+        assertEquals(EMAIL,response.getBody().getEmail());
+        assertEquals(NAME,response.getBody().getName());
+
     }
 
     @Test
