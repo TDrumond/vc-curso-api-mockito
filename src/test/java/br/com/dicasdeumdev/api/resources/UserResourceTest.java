79b9287270c4eb1644496e69249fef1e882dd711
TDrumond <thiago_drumond@outlook.com.br>
2022-11-15 16:35:56 -0300

    Teste para endpoint delete da classe UserResource implementado.

--- src/test/java/br/com/dicasdeumdev/api/resources/UserResourceTest.java
+++ src/test/java/br/com/dicasdeumdev/api/resources/UserResourceTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserResourceTest {
@@ -50,26 +50,26 @@ class UserResourceTest {
     @Test
     void whenFindByIdThenReturnSuccess() {
         when(service.findById(anyInt())).thenReturn(user);
-        when(mapper.map(any(),any())).thenReturn(userDTO);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
 
         ResponseEntity<UserDTO> response = resource.findById(ID);
 
         assertNotNull(response);
         assertNotNull(response.getBody());
         assertEquals(ResponseEntity.class, response.getClass());
-        assertEquals(UserDTO.class,response.getBody().getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
 
-        assertEquals(response.getBody().getId(),ID);
-        assertEquals(response.getBody().getEmail(),EMAIL);
-        assertEquals(response.getBody().getName(),NAME);
-        assertEquals(response.getBody().getPassword(),PASSWORD);
+        assertEquals(response.getBody().getId(), ID);
+        assertEquals(response.getBody().getEmail(), EMAIL);
+        assertEquals(response.getBody().getName(), NAME);
+        assertEquals(response.getBody().getPassword(), PASSWORD);
 
     }
 
     @Test
     void whenFindAllThenReturnAListOfUserDTO() {
         when(service.findAll()).thenReturn(List.of(user));
-        when(mapper.map(any(),any())).thenReturn(userDTO);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
 
         ResponseEntity<List<UserDTO>> response = resource.findAll();
 
@@ -77,13 +77,13 @@ class UserResourceTest {
         assertNotNull(response.getBody());
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(ResponseEntity.class, response.getClass());
-        assertEquals(ArrayList.class,response.getBody().getClass());
-        assertEquals(UserDTO.class,response.getBody().get(INDEX).getClass());
+        assertEquals(ArrayList.class, response.getBody().getClass());
+        assertEquals(UserDTO.class, response.getBody().get(INDEX).getClass());
 
-        assertEquals(ID,response.getBody().get(INDEX).getId());
-        assertEquals(EMAIL,response.getBody().get(INDEX).getEmail());
-        assertEquals(NAME,response.getBody().get(INDEX).getName());
-        assertEquals(PASSWORD,response.getBody().get(INDEX).getPassword());
+        assertEquals(ID, response.getBody().get(INDEX).getId());
+        assertEquals(EMAIL, response.getBody().get(INDEX).getEmail());
+        assertEquals(NAME, response.getBody().get(INDEX).getName());
+        assertEquals(PASSWORD, response.getBody().get(INDEX).getPassword());
     }
 
     @Test
@@ -102,9 +102,9 @@ class UserResourceTest {
     @Test
     void whenUpdateThenReturnSuccess() {
         when(service.update(userDTO)).thenReturn(user);
-        when(mapper.map(any(),any())).thenReturn(userDTO);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
 
-        ResponseEntity<UserDTO> response = resource.update(ID,userDTO);
+        ResponseEntity<UserDTO> response = resource.update(ID, userDTO);
 
         assertNotNull(response);
         assertNotNull(response.getBody());
@@ -113,14 +113,23 @@ class UserResourceTest {
         assertEquals(UserDTO.class, response.getBody().getClass());
 
 
-        assertEquals(ID,response.getBody().getId());
-        assertEquals(EMAIL,response.getBody().getEmail());
-        assertEquals(NAME,response.getBody().getName());
-
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(NAME, response.getBody().getName());
     }
 
     @Test
-    void delete() {
+    void deleteWithSuccess() {
+        doNothing().when(service).delete(any());
+
+        ResponseEntity<UserDTO> response = resource.delete(ID);
+
+        assertNotNull(response);
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+
+        verify(service, times(1)).delete(anyInt());
+
     }
 
     private void startUser() {
